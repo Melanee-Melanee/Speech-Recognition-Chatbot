2bddfddbfd9b4abe5c1fdeb21e82e80a7d5ad2fa
Melanee <74653444+Melanee-Melanee@users.noreply.github.com>
2023-12-04 10:09:06 +0330

    First Commit

---
+++ assistanttt.py
@@ -0,0 +1,54 @@
+import speech_recognition as sr
+import pyttsx3
+import openai
+
+openai.api_key = ""
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voices', voices[1].id)
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+conversation = ""
+user_name = "Melanee"
+bot_name = "Melanee's assistant"
+
+while True:
+    with mic as source:
+        print("\n Listening...")
+        r.adjust_for_ambient_noise(source, duration=0.2)
+        audio = r.listen(source)
+    print("no longer listening")
+
+    try:
+        user_input = r.recognize_google(audio)
+    except:
+        continue
+
+    prompt = user_name+":"+user_input + "\n"+bot_name+":"
+    conversation += prompt
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=conversation,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    response_str = response["choices"][0]["text"].replace("\n", "")
+    response_str =response_str.split(
+        user_name + ":" ,1)[0].split(bot_name+ ":",1)[0]
+
+    conversation+= response_str +"\n"
+    print(response_str)
+
+    engine.say(response_str)
+    engine.runAndWait()
+    
+
+
